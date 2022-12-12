5f112d5d8e1e8c8a6e0ee1b25a37540e355462d3
Jose Vega <josevega234@gmail.com>
2022-12-12 05:47:59 +0100

    move lines

--- mappings.lua
+++ mappings.lua
@@ -6,15 +6,21 @@ M.general = {
     ["<A-j>"] = { ":m .+1<CR>==", "move line down" },
     ["<A-k>"] = { ":m .-2<CR>==", "move line up" },
     ["<A-Down>"] = { ":m .+1<CR>==", "move line down" },
-    ["<A-Up>"] = { ":m .-2<CR>==", "move line up" },
+    ["<A-Up>"] = { ":m .-2<CR>==", "move line up" },-- close buffer + hide terminal buffer
+    ["<S-q>"] = {
+      function()
+        require("nvchad_ui.tabufline").close_buffer()
+      end,
+      "close buffer",
+    },
   },
   v = {
     ["s"] = { ":'<,'>BrowserSearch<cr>", "Browser Search" },
     -- move lines
-    ["<A-j>"] = { "<cmd> :m '>+1<CR>gv=gv", desc = "move line down" },
-    ["<A-k>"] = { "<cmd> :m '<-2<CR>gv=gv", desc = "move line up" },
-    ["<A-Down>"] = { "<cmd> :m '>+1<CR>gv=gv", desc = "move line down" },
-    ["<A-Up>"] = { "<cmd> :m '<-2<CR>gv=gv", desc = "move line up" },
+    ["<A-j>"] = { "<cmd> :m '>+1<CR>gv=gv", "move line down" },
+    ["<A-k>"] = { "<cmd> :m '<-2<CR>gv=gv", "move line up" },
+    ["<A-Down>"] = { "<cmd> :m '>+1<CR>gv=gv", "move line down" },
+    ["<A-Up>"] = { "<cmd> :m '<-2<CR>gv=gv", "move line up" },
   },
 }
 
