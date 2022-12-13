de59f2ca84648016aab4486d5be97cd150bc6e5d
Jose Vega <jevf@gft.com>
2022-12-13 14:12:37 +0100

    hotfix format on save

--- init.lua
+++ init.lua
@@ -52,7 +52,7 @@ vim.api.nvim_create_autocmd("WinClosed", {
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
-    vim.lsp.buf.formatting_sync()
+    vim.lsp.buf.format()
   end,
   nested = true,
 })
