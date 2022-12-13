146b93cb4116686e2c5fe8c04577584267f78974
Jose Vega <josevega234@gmail.com>
2022-12-13 01:43:30 +0100

    Terminal mappings

--- mappings.lua
+++ mappings.lua
@@ -7,14 +7,14 @@ M.general = {
     ["<A-j>"] = { ":m .+1<CR>==", "move line down" },
     ["<A-k>"] = { ":m .-2<CR>==", "move line up" },
     ["<A-Down>"] = { ":m .+1<CR>==", "move line down" },
-    ["<A-Up>"] = { ":m .-2<CR>==", "move line up" },-- close buffer + hide terminal buffer
+    ["<A-Up>"] = { ":m .-2<CR>==", "move line up" }, -- close buffer + hide terminal buffer
     ["<S-q>"] = {
       function()
         require("nvchad_ui.tabufline").close_buffer()
       end,
       "close buffer",
     },
-    ["<leader>gg"] = { "<cmd>LazyGit<cr>", "LazyGit"},
+    ["<leader>gg"] = { "<cmd>LazyGit<cr>", "LazyGit" },
   },
   v = {
     ["s"] = { ":'<,'>BrowserSearch<cr>", "Browser Search" },
@@ -23,6 +23,58 @@ M.general = {
     ["<A-k>"] = { "<cmd> :m '<-2<CR>gv=gv", "move line up" },
     ["<A-Down>"] = { "<cmd> :m '>+1<CR>gv=gv", "move line down" },
     ["<A-Up>"] = { "<cmd> :m '<-2<CR>gv=gv", "move line up" },
+  },
+}
+
+M.nvterm = {
+  plugin = true,
+
+  t = {
+    -- toggle in terminal mode
+    ["<leader>tf"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+
+    ["<leader>th"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "toggle horizontal term",
+    },
+
+    ["<leader>tv"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "toggle vertical term",
+    },
+  },
+
+  n = {
+    -- toggle in normal mode
+    ["<leader>tf"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+
+    ["<leader>th"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+      "toggle horizontal term",
+    },
+
+    ["<leader>tv"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+      "toggle vertical term",
+    },
   },
 }
 
