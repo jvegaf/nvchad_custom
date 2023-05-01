525bc2c7346c11a9857782a098fbfe5c65781e2b
Jose Vega <josevega234@gmail.com>
2023-05-01 22:32:24 +0200

    fix

--- plugins.lua
+++ plugins.lua
@@ -129,6 +129,7 @@ local plugins = {
   {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
+    event = "BufReadPre",
     config = function()
       vim.keymap.set("n", "zR", require("ufo").openAllFolds)
       vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
